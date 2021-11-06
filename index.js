1e1defa00dcd19cfe41c9cf588096c27232819b6
Franny Medico <m.francisca.medico@gmail.com>
2021-11-06 17:21:26 +0100

    modified index.js to deploy heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const initializeServer = require("./server/index");
 
 const robotsDB = process.env.MONGODB_ROBOTS;
 
-const port = process.env.SERVER_PORT_API || 6000;
+const port = process.env.PORT ?? process.env.SERVER_PORT_API ?? 6000;
 
 dataBaseInitializer(robotsDB);
 initializeServer(port);
