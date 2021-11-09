722cba1195f2add37334cc459207071665386b60
Franny Medico <m.francisca.medico@gmail.com>
2021-11-09 20:22:43 +0100

    modified index.js and put initalizer calls in an iife

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const initializeServer = require("./server/index");
 const robotsDB = process.env.MONGODB_ROBOTS;
 
 const port = process.env.PORT ?? process.env.SERVER_PORT_API ?? 6000;
-
-dataBaseInitializer(robotsDB);
-initializeServer(port);
+(async () => {
+  try {
+    await dataBaseInitializer(robotsDB);
+    initializeServer(port);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
