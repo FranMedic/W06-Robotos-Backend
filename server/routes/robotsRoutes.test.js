cce0659cde5ba92ea53359369eab095a0ca04c4e
Franny Medico <m.francisca.medico@gmail.com>
2021-11-10 15:37:16 +0100

    modifed test get and initialization of supertest test

--- server/routes/robotsRoutes.test.js
+++ server/routes/robotsRoutes.test.js
@@ -1,33 +1,42 @@
 require("dotenv").config();
 const debug = require("debug")("robots:testroute");
 const chalk = require("chalk");
-
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+
 const dataBaseInitializer = require("../../database");
 const Robot = require("../../database/models/robot");
-const { app, initializeServer } = require("..");
+const { initializeServer, app } = require("..");
 
 let server;
 let token;
 
-const request = supertest(app);
-const newrobot = {
-  name: "Wall-Y ",
-  image: "https://filmfilicos.com/wp-content/uploads/2020/06/wall-e.jpg",
-  features: {
-    velocity: 10,
-    resistance: 5,
-    creationData: "2019-11-24",
+const fakeRobots = [
+  {
+    _id: "618560f227992e4f22e6ae6b",
+    name: "MangoBB",
+    image:
+      "https://www.avforums.com/styles/avf/editorial/block//fac1e024ab104fa9939221aa7a592a15_3x3.jpg",
+    features: { velocity: 2, resistance: 5, creationData: "2015-06-05" },
   },
-};
+  {
+    _id: "6185611d27992e4f22e6ae6c",
+    name: "Manivela",
+    image:
+      "https://images.hobbydatabase.com/processed_uploads/subject_photo/subject_photo/image/40952/1530552727-31404-3391/Fender_20Pinwheeler_large.jpg",
+    features: { velocity: 8, resistance: 7, creationData: "2012-11-24" },
+  },
+];
+
+jest.setTimeout(10000);
+
+const request = supertest(app);
 
 beforeAll(async () => {
   await dataBaseInitializer(process.env.MONGODB_TESTS);
+  await Robot.deleteMany({});
   server = await initializeServer(process.env.SERVER_PORT_TEST);
-});
 
-beforeEach(async () => {
   const loginResponse = await request
     .post("/users/login")
     .send({ username: "thegreatfranny", password: "zoylamazmejor" })
@@ -35,41 +44,20 @@ beforeEach(async () => {
 
   token = loginResponse.body.token;
   console.log(token);
-
-  await Robot.deleteMany();
-  await Robot.create({
-    _id: "618560f227992e4f22e6ae6b",
-    name: "MangoBB",
-    image:
-      "https://www.avforums.com/styles/avf/editorial/block//fac1e024ab104fa9939221aa7a592a15_3x3.jpg",
-    features: {
-      velocity: 2,
-      resistance: 5,
-      creationData: "2015-06-05",
-    },
-  });
-  await Robot.create({
-    _id: "6185611d27992e4f22e6ae6c",
-    name: "Manivela",
-    image:
-      "https://images.hobbydatabase.com/processed_uploads/subject_photo/subject_photo/image/40952/1530552727-31404-3391/Fender_20Pinwheeler_large.jpg",
-    features: {
-      velocity: 8,
-      resistance: 7,
-      creationData: "2012-11-24",
-    },
-  });
 });
 
-afterAll(async () => {
-  await mongoose.connection.on("close", () => {
-    debug(chalk.red("Connection with database finished, now i die"));
+beforeEach(async () => {
+  await Robot.deleteMany({});
+  await Robot.create(fakeRobots[0]);
+  await Robot.create(fakeRobots[1]);
+});
+
+afterAll((done) => {
+  server.close(async () => {
+    await mongoose.connection.close();
+    done();
+    debug(chalk.red("server closed NOW YOU DIE!"));
   });
-  await mongoose.connection.close();
-  await server.on("close", () => {
-    debug(chalk.red("Connection with server finished, and now you die"));
-  });
-  await server.close();
 });
 
 describe("Given a /robots router", () => {
@@ -81,26 +69,27 @@ describe("Given a /robots router", () => {
         .get("/robots")
         .set("Authorization", `Bearer ${token}`)
         .expect(200);
-      const expectedRobot = {
-        __v: 0,
-        _id: "618560f227992e4f22e6ae6b",
-        name: "MangoBB",
-        image:
-          "https://www.avforums.com/styles/avf/editorial/block//fac1e024ab104fa9939221aa7a592a15_3x3.jpg",
-        features: {
-          velocity: 2,
-          resistance: 5,
-          creationData: "2015-06-05",
-        },
-      };
 
+      const fakeRobotsWithId = fakeRobots.map((fakeRobot) => {
+        const fakeRobotWithId = {
+          ...fakeRobot,
+          // eslint-disable-next-line no-underscore-dangle
+          id: fakeRobot._id,
+        };
+        // eslint-disable-next-line no-underscore-dangle
+        delete fakeRobotWithId._id;
+
+        return fakeRobotWithId;
+      });
+      console.log(fakeRobotsWithId);
       expect(body).toHaveLength(2);
-      expect(body).toContainEqual(expectedRobot);
+      expect(body).toContainEqual(fakeRobotsWithId[1]);
     });
   });
-
+});
+/*
   describe("when a post request to /robots/create arrives with a robot", () => {
-    test("then it should respond with an status 201", async () => {
+    test.skip("then it should respond with an status 201", async () => {
       const { body } = await request
         .get("/robots/create")
         .set("Authorization", `Bearer ${token}`)
@@ -109,4 +98,4 @@ describe("Given a /robots router", () => {
       console.log(`${body}patata`);
     });
   });
-});
+*/
